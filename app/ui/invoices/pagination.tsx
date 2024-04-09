a947c00bd190cfde290932e130d0b365663aa074
Pedro Pablo Contreras Parra <pedropablo.contrerasparra@everis.nttdata.com>
2024-04-09 17:06:06 -0500

    ss

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -18,20 +18,8 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
     return `${pathname}?${params.toString()}`;
   };
 
-<<<<<<< HEAD
-  const pathname = usePathname();
-  const searchParams = useSearchParams();
-  const currentPage = Number(searchParams.get('page')) || 1;
-  const allPages = generatePagination(currentPage, totalPages);
-  const createPageURL = (pageNumber: number | string) => {
-    const params = new URLSearchParams(searchParams);
-    params.set('page', pageNumber.toString());
-    return `${pathname}?${params.toString()}`;
-  };
-=======
   const allPages = generatePagination(currentPage, totalPages);
 
->>>>>>> upstream/main
   return (
     <>
       {/* NOTE: comment in this code when you get to this point in the course */}
--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -3,20 +3,12 @@
 import { useDebouncedCallback } from 'use-debounce';
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
-<<<<<<< HEAD
-import { useDebouncedCallback } from 'use-debounce';
-=======
 
 
->>>>>>> upstream/main
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
-<<<<<<< HEAD
-=======
-
->>>>>>> upstream/main
   const handleSearch = useDebouncedCallback((term) => {
     console.log(`Searching... ${term}`);
     const params = new URLSearchParams(searchParams);
@@ -27,11 +19,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
       params.delete('query');
     }
     replace(`${pathname}?${params.toString()}`);
-<<<<<<< HEAD
-  }, 300);
-=======
   }, 300)
->>>>>>> upstream/main
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
@@ -40,12 +28,8 @@ export default function Search({ placeholder }: { placeholder: string }) {
       </label>
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-<<<<<<< HEAD
-        placeholder={placeholder} onChange={(e) => {
-=======
         placeholder={placeholder}
         onChange={(e) => {
->>>>>>> upstream/main
           handleSearch(e.target.value);
         }}
         defaultValue={searchParams.get('query')?.toString()}
